c34b8c79e4756f30b6b9e06d151d0d31cef839f2
Duong Tran <dtducas@gmail.com>
2025-08-11 00:24:53 +0700

    🚀 perf(button) [FE]: double the 'no' button's escape speed

--- script.js
+++ script.js
@@ -2,19 +2,14 @@ let yesButton = document.getElementById("yes");
 let noButton = document.getElementById("no");
 let questionText = document.getElementById("question");
 let mainImage = document.getElementById("mainImage");
-
 const params = new URLSearchParams(window.location.search);
 let username = params.get("name");
-
 const maxLength = 20;
 const safeUsername = username ? username.substring(0, maxLength) : "???";
-
 if (username) {
   questionText.innerText = questionText.innerText + safeUsername;
 }
-
 let clickCount = 0;
-
 const noTexts = [
   "? Bé nghiêm túc chứ...",
   "Suy nghĩ lại nhé?",
@@ -22,35 +17,27 @@ const noTexts = [
   "Tui sẽ buồn lắm...",
   "Không được :(",
 ];
-
 noButton.addEventListener("click", function () {
   clickCount++;
-
   let yesSize = 1 + clickCount * 1.2;
   yesButton.style.transform = `scale(${yesSize})`;
-
-  let noOffset = clickCount * 50;
+  let noOffset = clickCount * 100;
   noButton.style.transform = `translateX(${noOffset}px)`;
-
   let moveUp = clickCount * 25;
   mainImage.style.transform = `translateY(-${moveUp}px)`;
   questionText.style.transform = `translateY(-${moveUp}px)`;
-
   if (clickCount <= 5) {
     noButton.innerText = noTexts[clickCount - 1];
   }
-
   if (clickCount === 1) mainImage.src = "img/shocked.png";
   if (clickCount === 2) mainImage.src = "img/think.png";
   if (clickCount === 3) mainImage.src = "img/angry.png";
   if (clickCount === 4) mainImage.src = "img/crying.png";
   if (clickCount >= 5) mainImage.src = "img/crying.png";
 });
-
 const loveTest = `!!!Yêu bé!! ( >᎑<)♡︎ᐝ  ${
   username ? `${safeUsername}  ♡︎ᐝ(>᎑< )` : ""
 }`;
-
 yesButton.addEventListener("click", function () {
   document.body.innerHTML = `
         <div class="yes-screen">
@@ -58,8 +45,6 @@ yesButton.addEventListener("click", function () {
             <img src="img/hug.png" alt="ôm" class="yes-image">
         </div>
     `;
-
   document.querySelector(".yes-text").innerText = loveTest;
-
   document.body.style.overflow = "hidden";
 });
